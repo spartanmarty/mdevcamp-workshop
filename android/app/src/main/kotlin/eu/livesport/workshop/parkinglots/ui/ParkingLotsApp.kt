714c7e5ba5211567c1230ae0f498fc18015800ec
martin.holodniok <martin.holodniok@livesport.eu>
2025-05-12 12:36:21 +0200

    theme

--- android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/ParkingLotsApp.kt
+++ android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/ParkingLotsApp.kt
@@ -2,23 +2,29 @@ package eu.livesport.workshop.parkinglots.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import eu.livesport.workshop.parkinglots.navigation.Navigation
+import eu.livesport.workshop.parkinglots.ui.theme.parkingLotsColors
 
 @Composable
 fun ParkingLotsApp() {
-    val navController = rememberNavController()
+    MaterialTheme(
+        colorScheme = parkingLotsColors,
+    ) {
+        val navController = rememberNavController()
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = { BottomNavBar(navController) }
-    ) { padding ->
-        Navigation(
-            navController = navController,
-            modifier = Modifier.padding(padding)
-        )
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = { BottomNavBar(navController) }
+        ) { padding ->
+            Navigation(
+                navController = navController,
+                modifier = Modifier.padding(padding)
+            )
+        }
     }
 }
---
+++ android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/theme/ParkingLotsTheme.kt
@@ -0,0 +1,22 @@
+package eu.livesport.workshop.parkinglots.ui.theme
+
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+//#92e156
+//#e8f6dd
+
+val parkingLotsColors = lightColorScheme(
+    primary = Color(0xFF92E156),
+    onPrimary = Color.Black,
+    secondary = Color(0xFF92E156),
+    onSecondary = Color.White,
+    background = Color(0xFFFFFFFF),
+    onBackground = Color(0xFF000000),
+    surface = Color(0xFFFFFFFF),
+    onSurface = Color(0xFF000000),
+    surfaceVariant = Color(0xFFE8F6DD),
+    onSurfaceVariant = Color(0xFF254421),
+    secondaryContainer = Color(0xFF92E156),
+    onSecondaryContainer = Color.White,
+)
