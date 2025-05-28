d8afd4a7cf34d5a8d0712c18b929c15daf9912f7
Jan Pokorný <jan.pokorny@livesport.eu>
2025-05-28 18:05:25 +0200

    Delete favorites

--- ios/iosApp/components/ParkingDetail.swift
+++ ios/iosApp/components/ParkingDetail.swift
@@ -23,21 +23,7 @@ struct ParkingDetail: View {
 
 			ProhibitionRow(items: model.prohibitions)
 
-			Button(
-				action: { closure(model) },
-				label: {
-					Text(model.isFavorite ? "favorites_remove" : "favorites_add")
-						.font(.callout)
-						.fontWeight(.regular)
-						.padding(.vertical, 16)
-						.frame(maxWidth: .infinity)
-						.background(Color("primary"))
-						.cornerRadius(24)
-				}
-			)
-			.buttonStyle(.plain)
-			.padding(.vertical, 16)
-			.padding(.horizontal, 24)
+			// TODO - Button - Add to favorites / Remove from favorites
 		}
 		.padding(.horizontal, 16)
 		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
