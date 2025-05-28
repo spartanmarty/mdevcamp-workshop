0aaec5b526ed39bee2b932c3dcc6330fe410679d
Patrik Mucha <patrik.mucha@livesport.eu>
2025-05-28 18:10:29 +0200

    Clean up ParkingLot detail screen (Compose).

--- android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/screens/ParkingLotDetailScreen.kt
+++ android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/screens/ParkingLotDetailScreen.kt
@@ -1,136 +1,27 @@
 package eu.livesport.workshop.parkinglots.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
-import eu.livesport.workshop.parkinglots.R
 import eu.livesport.workshop.parkinglots.repository.model.ParkingLot
-import eu.livesport.workshop.parkinglots.ui.common.Error
-import eu.livesport.workshop.parkinglots.ui.common.LabelValueText
-import eu.livesport.workshop.parkinglots.ui.common.Loading
-import eu.livesport.workshop.parkinglots.viewmodel.ParkingLotDetailViewModel
-import eu.livesport.workshop.parkinglots.viewmodel.State
-import org.koin.mp.KoinPlatform
+import eu.livesport.workshop.parkinglots.repository.model.ParkingProhibitions
 
 @Composable
 fun ParkingLotDetailScreen(
     parkingLotId: String,
-    viewModel: ParkingLotDetailViewModel = createViewModel(),
 ) {
-    LaunchedEffect(parkingLotId) {
-        viewModel.loadParkingLotDetail(parkingLotId)
-    }
-
-    val state: State by viewModel.state.collectAsStateWithLifecycle()
-
-    when (state) {
-        is State.Loading -> Loading()
-
-        is State.Data ->
-            (state as State.Data).parkingLots
-                .firstOrNull()
-                ?.let { parkingLot ->
-                    DetailContent(
-                        parkingLot = parkingLot,
-                    )
-                }
-                ?: Error(State.Error(State.Error.Type.NO_DATA_FOUND))
-
-        is State.Error -> Error(state as State.Error)
-    }
-}
-
-@Composable
-private fun DetailContent(
-    parkingLot: ParkingLot,
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 18.dp)
-            .padding(top = 24.dp)
-    ) {
-        Text(
-            text = parkingLot.name,
-            style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(bottom = 24.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-
-        LabelValueText(
-            label = stringResource(id = R.string.label_address),
-            value = parkingLot.address ?: stringResource(id = R.string.value_unknown),
-            style = MaterialTheme.typography.bodyLarge,
-        )
-
-        Spacer(Modifier.height(8.dp))
-        LabelValueText(
-            label = stringResource(id = R.string.label_capacity),
-            value = parkingLot.capacity.toString(),
-            style = MaterialTheme.typography.bodyLarge,
-        )
-
-        Spacer(Modifier.height(16.dp))
-        Text(
-            text = stringResource(id = R.string.label_prohibitions),
-            style = MaterialTheme.typography.headlineSmall,
-            modifier = Modifier.padding(bottom = 4.dp)
-        )
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            parkingLot.prohibitions.forEach {
-                ProhibitionIcon()
-            }
-        }
-    }
-}
-
-@Composable
-private fun ProhibitionIcon() {
-    Box(
-        modifier = Modifier
-            .size(48.dp)
-            .background(MaterialTheme.colorScheme.surfaceVariant, shape = RoundedCornerShape(12.dp)),
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
-            painter = painterResource(id = R.drawable.icon_warning),
-            contentDescription = "Prohibition",
-            tint = Color(0xFF132925),
-            modifier = Modifier.size(32.dp)
-        )
-    }
-}
-
-@Composable
-private fun createViewModel(): ParkingLotDetailViewModel =
-    viewModel<ParkingLotDetailViewModel>(
-        factory = KoinPlatform.getKoin().get<ParkingLotDetailViewModel.Factory>(),
+    val dummyParkingLot = ParkingLot(
+        id = "123",
+        name = "Dummy Parking Lot",
+        address = "123 Dummy Street, City, Country",
+        capacity = 123,
+        covered = false,
+        isFavorite = false,
+        prohibitions = listOf(ParkingProhibitions.LPG_CNG, ParkingProhibitions.TRUCK),
     )
+
+    // TODO: Implement the UI for displaying parking lot details
+    // 1. Parking lot name as a screen title
+    // 2. Address
+    // 3. Capacity
+    // 4. Covered status
+    // 5. List of prohibitions using icons
+}
