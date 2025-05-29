3abb077ff55e163a9fd7f79894d5b40cd18ec659
Patrik Mucha <patrik.mucha@livesport.eu>
2025-05-29 10:02:33 +0200

    Optimize imports.

--- android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/components/FilterBar.kt
+++ android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/components/FilterBar.kt
@@ -3,24 +3,15 @@ package eu.livesport.workshop.parkinglots.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.FilterChipDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import eu.livesport.workshop.parkinglots.R
 import eu.livesport.workshop.parkinglots.repository.model.ParkingPolicyFilter
 
 @Composable
