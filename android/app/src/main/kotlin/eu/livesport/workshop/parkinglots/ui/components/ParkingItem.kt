9fb91144233d336c241086b52cbea8a81302da4b
martin.holodniok <martin.holodniok@livesport.eu>
2025-05-29 09:51:27 +0200

    remove composables for filter chip and ParkingItem

--- android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/components/ParkingItem.kt
+++ android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/components/ParkingItem.kt
@@ -23,51 +23,7 @@ fun ParkingItem(
     modifier: Modifier = Modifier,
     onItemClick: (ParkingLot) -> Unit = {},
 ) {
-    Card(
-        modifier = modifier
-            .padding(8.dp)
-            .fillMaxWidth(),
-        onClick = { onItemClick(model) },
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
-        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(8.dp)
-        ) {
-            ParkingName(model.name)
-            ParkingInfo(model)
-        }
-
-    }
-}
-
-@Composable
-fun ParkingName(parkingName: String) {
-    Text(
-        text = parkingName,
-        style = MaterialTheme.typography.titleMedium,
-    )
-}
-
-@Composable
-fun ParkingInfo(model: ParkingLot) {
-    LabelValueText(
-        label = stringResource(R.string.label_capacity),
-        value = model.capacity.toString(),
-    )
-    LabelValueText(
-        label = stringResource(R.string.label_address),
-        value = model.address.orEmpty().ifBlank { stringResource(R.string.value_unknown) },
-    )
-    LabelValueText(
-        label = stringResource(R.string.label_covered),
-        value = when (model.covered) {
-            true -> stringResource(R.string.covered_yes)
-            false -> stringResource(R.string.covered_no)
-            null -> stringResource(R.string.value_unknown)
-        },
-    )
+    Text("ParkingItem")
 }
 
 @Preview
