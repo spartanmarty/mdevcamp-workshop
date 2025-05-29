4cfad4e4983551462f93564789770fb9661a7728
Patrik Mucha <patrik.mucha@livesport.eu>
2025-05-29 17:38:36 +0200

    Bring back empty Koin repository module definition.

---
+++ shared/src/commonMain/kotlin/eu/livesport/workshop/parkinglots/internal/di/SharedRepositoryModule.kt
@@ -0,0 +1,9 @@
+package eu.livesport.workshop.parkinglots.internal.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+internal val sharedRepositoryModule: Module =
+    module {
+        // TODO: Add repository bindings here
+    }
