65072e3eb6c598669d72d43642b695c6de4fca71
Patrik Mucha <patrik.mucha@livesport.eu>
2025-05-29 16:37:26 +0200

    Revert "Optimize imports."
    This reverts commit 3abb077ff55e163a9fd7f79894d5b40cd18ec659.

--- android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/components/ParkingItem.kt
+++ android/app/src/main/kotlin/eu/livesport/workshop/parkinglots/ui/components/ParkingItem.kt
@@ -1,10 +1,20 @@
 package eu.livesport.workshop.parkinglots.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import eu.livesport.workshop.parkinglots.R
 import eu.livesport.workshop.parkinglots.repository.model.ParkingLot
+import eu.livesport.workshop.parkinglots.ui.common.LabelValueText
 import eu.livesport.workshop.parkinglots.ui.theme.ParkingLotsTheme
 
 @Composable
